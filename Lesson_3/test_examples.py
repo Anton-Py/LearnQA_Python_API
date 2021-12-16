5ea130f6355544cb02559c51da2f9e904d2f91ea
a.lyakh <a.lyakh@movavi.com>
2021-12-17 01:38:15 +0700

    add lesson 3

---
+++ Lesson_3/test_examples.py
@@ -0,0 +1,16 @@
+class TestExample:
+    def test_check_math(self):
+        a = 5
+        b = 9
+        expected_sum = 14
+        assert a + b == expected_sum, f"Что то пошло не так {expected_sum}"
+
+    def test_check_math2(self):
+        a = 5
+        b = 11
+        expected_sum = 14
+        assert a + b == expected_sum, f"Что то пошло не так {expected_sum}"
+
+
+# python -m pytest Lesson_3/test_examples.py -k "test_check_math"
+
