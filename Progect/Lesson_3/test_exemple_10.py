55a34e5954a992a87d67251aa0a4a49f70813c62
a.lyakh <a.lyakh@movavi.com>
2021-12-20 22:01:11 +0700

    fix

--- Progect/Lesson_3/test_exemple_10.py
+++ Progect/Lesson_3/test_exemple_10.py
@@ -3,3 +3,5 @@ class TestExempleShortPhrase():
         phrase = input("Set a phrase: ")
         assert len(phrase) < 15, f"Длина фразы: '{phrase}' больше 15 символов"
 
+#  pytest -s Lesson_3/test_exemple_10.py -k "test_short_phrase"
+
