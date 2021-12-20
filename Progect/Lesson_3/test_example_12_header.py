0639f36a00571ef5b740295bc779d9ab1c30c092
a.lyakh <a.lyakh@movavi.com>
2021-12-21 01:44:18 +0700

    add test_12_header

--- Progect/Lesson_3/test_example_12_header.py
+++ Progect/Lesson_3/test_example_12_header.py
@@ -1,12 +1,13 @@
 import pytest
 import requests
+import json
 
 
-class TestCookie:
-    def test_cookie(self):
-        response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
-        cookie = dict(response.cookies)
-        print(cookie)
-        assert cookie['HomeWork'] == "hw_value", "The cookie parameter is not correct"
+class TestHeader:
+    def test_header(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_header")
+        print(response.headers)
+        print(response.headers["x-secret-homework-header"])
+        assert "x-secret-homework-header" in response.headers, "There is no x-secret-homework-header in the response"
 
-#  pytest -s Lesson_3/test_example_11_cookie.py -k "test_cookie"
+#  pytest -s Lesson_3/test_example_12_header.py -k "test_header"
