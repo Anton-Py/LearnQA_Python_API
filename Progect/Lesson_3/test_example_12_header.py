aad191b81723c0f2f1bcec6b64ca2438c91d1f8f
a.lyakh <a.lyakh@movavi.com>
2021-12-21 01:47:29 +0700

    fix

--- Progect/Lesson_3/test_example_12_header.py
+++ Progect/Lesson_3/test_example_12_header.py
@@ -7,7 +7,7 @@ class TestHeader:
     def test_header(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_header")
         print(response.headers)
-        print(response.headers["x-secret-homework-header"])
-        assert "x-secret-homework-header" in response.headers, "There is no x-secret-homework-header in the response"
+        header = response.headers
+        assert header["x-secret-homework-header"] == "Some secret value", "There is no x-secret-homework-header in the response"
 
 #  pytest -s Lesson_3/test_example_12_header.py -k "test_header"
