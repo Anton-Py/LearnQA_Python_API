322160cc6e24665945c8ed7a1e0458eb986ed0be
a.lyakh <a.lyakh@movavi.com>
2021-12-21 01:19:05 +0700

    fix

--- Progect/Lesson_3/test_example_11.py
+++ Progect/Lesson_3/test_example_11.py
@@ -6,7 +6,7 @@ class TestCookie:
     def test_cookie(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
         cookie = dict(response.cookies)
-        print(cookie['HomeWork'])
+        print(cookie)
         assert cookie['HomeWork'] == "hw_value", "The cookie parameter is not correct"
 
 #  pytest -s Lesson_3/test_example_11.py -k "test_cookie"
